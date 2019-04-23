ba255c89295fb5b84c2ad2fdff4580a0b4801e6c
parvizrovshanaliyev <parvizra@code.edu.az>
2019-04-23 18:59:06 +0400

    nav readey he,de responsive

--- js/app.js
+++ js/app.js
@@ -1,22 +1,39 @@
-$(document).ready(function(){
-    console.log("ssss");
-    /*------ Sticky menu start ------*/
-	var $window = $(window);
-	$window.on('scroll', function () {
-		var scroll = $window.scrollTop();
-		if (scroll < 300) {
-			$(".sticky").removeClass("is-sticky");
-		} else {
-			$(".sticky").addClass("is-sticky");
-		}
-	});
-    /*------ Sticky menu end ------*/
-    
-    // /*----- sticky sidebar start -----*/
-	// $('.sticky-sidebar').stickySidebar({
-	// 	topSpacing: 120,
-	// 	bottomSpacing: 20,
-	// 	minWidth: 992,
-	// }); 
-	// /*----- sticky sidebar end -----*/
+$(document).ready(function() {
+  console.log("ssss");
+  /*------ Sticky menu start ------*/
+  var $window = $(window);
+  $window.on("scroll", function() {
+    var scroll = $window.scrollTop();
+    var nav = $("#nav");
+    if (scroll < 300) {
+      $(".sticky").removeClass("is-sticky");
+    } else {
+      $(".sticky").addClass("is-sticky");
+    }
+    if (scroll < 300 || nav.hasClass("active")) {
+      nav.removeClass("active");
+    }
+  });
+  /*------ Sticky menu end ------*/
+
+  // /*----- sticky sidebar start -----*/
+  // $('.sticky-sidebar').stickySidebar({
+  // 	topSpacing: 120,
+  // 	bottomSpacing: 20,
+  // 	minWidth: 992,
+  // });
+  // /*----- sticky sidebar end -----*/
+
+  //mobile mune click
+
+  $(".bars").on("click", function(e) {
+    e.preventDefault();
+    // alert("salam seni sinsi");
+    var nav = $("#nav");
+    if (nav.hasClass("active")) {
+      nav.removeClass("active");
+    } else {
+      nav.addClass("active");
+    }
+  });
 });
