a1e296e5f8c0ac0cd9467e2ae578dfa66418e3ff
parvizrovshanaliyev <parvizra@code.edu.az>
2019-04-25 15:49:27 +0400

    trend product slider and product view ready

--- js/app.js
+++ js/app.js
@@ -38,7 +38,74 @@ $(document).ready(function() {
   });
 
 
-  /*------  slider active start ------*/
-	
-	/*------  slider active end ------*/
+  /*------  slider trending active start ------*/
+	// var slider = $("#lightSlider").lightSlider();
+  //   slider.goToSlide(3);
+  //   slider.goToPrevSlide();
+  //   slider.goToNextSlide();
+  //   slider.getCurrentSlideCount(4);
+  //   slider.refresh();
+  //   slider.play(); 
+  //   slider.pause();
+
+
+  /*------  slider active end ------*/
+  /*------  trendin product  ------*/
+  $("#lightSlider").lightSlider({
+    item: 3,
+    autoWidth: false,
+    slideMove: 1, // slidemove will be 1 if loop is true
+    slideMargin: 10,
+
+    addClass: '',
+    mode: "slide",
+    useCSS: true,
+    cssEasing: 'ease', //'cubic-bezier(0.25, 0, 0.25, 1)',//
+    easing: 'linear', //'for jquery animation',////
+
+    speed: 400, //ms'
+    auto: false,
+    loop: false,
+    slideEndAnimation: true,
+    pause: 2000,
+
+    keyPress: false,
+    controls: true,
+    prevHtml: '<',
+    nextHtml: '>',
+
+    rtl:false,
+    adaptiveHeight:false,
+
+    vertical:false,
+    verticalHeight:500,
+    vThumbWidth:100,
+
+    thumbItem:10,
+    pager: true,
+    gallery: false,
+    galleryMargin: 5,
+    thumbMargin: 5,
+    currentPagerPosition: 'middle',
+
+    enableTouch:true,
+    enableDrag:true,
+    freeMove:true,
+    swipeThreshold: 40,
+
+    responsive : [],
+
+    onBeforeStart: function (el) {},
+    onSliderLoad: function (el) {},
+    onBeforeSlide: function (el) {},
+    onAfterSlide: function (el) {},
+    onBeforeNextSlide: function (el) {},
+    onBeforePrevSlide: function (el) {}
 });
+  /*------  trendin product  ------*/
+
+
+}); 
+
+  
+
