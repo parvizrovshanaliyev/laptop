f1ca953a3bdfd7d47a59226a92d6d61a784dc543
parvizrovshanaliyev <parvizra@code.edu.az>
2019-04-25 17:32:25 +0400

    deli deli seyler 3 section tab menuda

--- js/app.js
+++ js/app.js
@@ -37,75 +37,189 @@ $(document).ready(function() {
     }
   });
 
-
   /*------  slider trending active start ------*/
-	// var slider = $("#lightSlider").lightSlider();
+  // var slider = $("#lightSlider").lightSlider();
   //   slider.goToSlide(3);
   //   slider.goToPrevSlide();
   //   slider.goToNextSlide();
   //   slider.getCurrentSlideCount(4);
   //   slider.refresh();
-  //   slider.play(); 
+  //   slider.play();
   //   slider.pause();
 
-
   /*------  slider active end ------*/
-  /*------  trendin product  ------*/
-  $("#lightSlider").lightSlider({
-    item: 3,
-    autoWidth: false,
-    slideMove: 1, // slidemove will be 1 if loop is true
-    slideMargin: 10,
+  /*------  section tabs ------*/
+  $(".js-tabs-link").aniTabs({
+    animation: "slide",
 
-    addClass: '',
-    mode: "slide",
-    useCSS: true,
-    cssEasing: 'ease', //'cubic-bezier(0.25, 0, 0.25, 1)',//
-    easing: 'linear', //'for jquery animation',////
+    slideDirection: "left",
+    animationSpeed: 1000,
+    autoHeight: false
+  });
+    /*------  trendin product  ------*/
+    $("#lightSlider").lightSlider({
+      item: 3,
+      autoWidth: false,
+      slideMove: 1, // slidemove will be 1 if loop is true
+      slideMargin: 10,
+  
+      addClass: "",
+      mode: "slide",
+      useCSS: true,
+      cssEasing: "ease", //'cubic-bezier(0.25, 0, 0.25, 1)',//
+      easing: "linear", //'for jquery animation',////
+  
+      speed: 400, //ms'
+      auto: false,
+      loop: false,
+      slideEndAnimation: true,
+      pause: 2000,
+  
+      keyPress: false,
+      controls: true,
+      prevHtml: "<",
+      nextHtml: ">",
+  
+      rtl: false,
+      adaptiveHeight: false,
+  
+      vertical: false,
+      verticalHeight: 500,
+      vThumbWidth: 100,
+  
+      thumbItem: 10,
+      pager: true,
+      gallery: false,
+      galleryMargin: 5,
+      thumbMargin: 5,
+      currentPagerPosition: "middle",
+  
+      enableTouch: true,
+      enableDrag: true,
+      freeMove: true,
+      swipeThreshold: 40,
+  
+      responsive: [],
+  
+      onBeforeStart: function(el) {},
+      onSliderLoad: function(el) {},
+      onBeforeSlide: function(el) {},
+      onAfterSlide: function(el) {},
+      onBeforeNextSlide: function(el) {},
+      onBeforePrevSlide: function(el) {}
+    });
+    /*------  trendin product  ------*/
 
-    speed: 400, //ms'
-    auto: false,
-    loop: false,
-    slideEndAnimation: true,
-    pause: 2000,
+    /*------  news product  ------*/
+    $("#lightSlider2").lightSlider({
+      item: 3,
+      autoWidth: false,
+      slideMove: 1, // slidemove will be 1 if loop is true
+      slideMargin: 10,
+  
+      addClass: "",
+      mode: "slide",
+      useCSS: true,
+      cssEasing: "ease", //'cubic-bezier(0.25, 0, 0.25, 1)',//
+      easing: "linear", //'for jquery animation',////
+  
+      speed: 400, //ms'
+      auto: false,
+      loop: false,
+      slideEndAnimation: true,
+      pause: 2000,
+  
+      keyPress: false,
+      controls: true,
+      prevHtml: "<",
+      nextHtml: ">",
+  
+      rtl: false,
+      adaptiveHeight: false,
+  
+      vertical: false,
+      verticalHeight: 500,
+      vThumbWidth: 100,
+  
+      thumbItem: 10,
+      pager: true,
+      gallery: false,
+      galleryMargin: 5,
+      thumbMargin: 5,
+      currentPagerPosition: "middle",
+  
+      enableTouch: true,
+      enableDrag: true,
+      freeMove: true,
+      swipeThreshold: 40,
+  
+      responsive: [],
+  
+      onBeforeStart: function(el) {},
+      onSliderLoad: function(el) {},
+      onBeforeSlide: function(el) {},
+      onAfterSlide: function(el) {},
+      onBeforeNextSlide: function(el) {},
+      onBeforePrevSlide: function(el) {}
+    });
+    /*------  trendin product  ------*/
 
-    keyPress: false,
-    controls: true,
-    prevHtml: '<',
-    nextHtml: '>',
+    /*------  sale product  ------*/
+    $("#lightSlider3").lightSlider({
+      item: 3,
+      autoWidth: false,
+      slideMove: 1, // slidemove will be 1 if loop is true
+      slideMargin: 10,
+  
+      addClass: "",
+      mode: "slide",
+      useCSS: true,
+      cssEasing: "ease", //'cubic-bezier(0.25, 0, 0.25, 1)',//
+      easing: "linear", //'for jquery animation',////
+  
+      speed: 400, //ms'
+      auto: false,
+      loop: false,
+      slideEndAnimation: true,
+      pause: 2000,
+  
+      keyPress: false,
+      controls: true,
+      prevHtml: "<",
+      nextHtml: ">",
+  
+      rtl: false,
+      adaptiveHeight: false,
+  
+      vertical: false,
+      verticalHeight: 500,
+      vThumbWidth: 100,
+  
+      thumbItem: 10,
+      pager: true,
+      gallery: false,
+      galleryMargin: 5,
+      thumbMargin: 5,
+      currentPagerPosition: "middle",
+  
+      enableTouch: true,
+      enableDrag: true,
+      freeMove: true,
+      swipeThreshold: 40,
+  
+      responsive: [],
+  
+      onBeforeStart: function(el) {},
+      onSliderLoad: function(el) {},
+      onBeforeSlide: function(el) {},
+      onAfterSlide: function(el) {},
+      onBeforeNextSlide: function(el) {},
+      onBeforePrevSlide: function(el) {}
+    });
+    /*------  trendin product  ------*/
+  /*------  section tabs ------*/
 
-    rtl:false,
-    adaptiveHeight:false,
 
-    vertical:false,
-    verticalHeight:500,
-    vThumbWidth:100,
-
-    thumbItem:10,
-    pager: true,
-    gallery: false,
-    galleryMargin: 5,
-    thumbMargin: 5,
-    currentPagerPosition: 'middle',
-
-    enableTouch:true,
-    enableDrag:true,
-    freeMove:true,
-    swipeThreshold: 40,
-
-    responsive : [],
-
-    onBeforeStart: function (el) {},
-    onSliderLoad: function (el) {},
-    onBeforeSlide: function (el) {},
-    onAfterSlide: function (el) {},
-    onBeforeNextSlide: function (el) {},
-    onBeforePrevSlide: function (el) {}
-});
-  /*------  trendin product  ------*/
-
-
-}); 
 
   
-
+});
